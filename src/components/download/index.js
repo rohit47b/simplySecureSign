bdee946b6b841ac11ec62aa80571d1d014bbb0fe
virendra patidar <virendra.patidar@47billion.com>
2019-04-10 11:57:37 +0530

    Change download fule name

--- src/components/download/index.js
+++ src/components/download/index.js
@@ -15,7 +15,7 @@ class Download extends PureComponent {
                 <div className="err-body">
                     <h1>Simply Secure eSign</h1>
                     <p>Please click on below button for download source code</p>
-                    <Button type="button" variant="contained" className="btn btn-success"><a href={file} >Download</a> </Button>
+                    <Button type="button" variant="contained" className="btn btn-success"><a href={file} download="SimplySecureSign" >Download</a> </Button>
                 </div>
             </div>
             </div>
