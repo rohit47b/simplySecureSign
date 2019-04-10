c8fca7d00c387d4b05beec69d53d3b2701ff8564
rohit47b <41292591+rohit47b@users.noreply.github.com>
2019-04-10 11:38:53 +0530

    Update index.js
    Change path

--- src/components/download/index.js
+++ src/components/download/index.js
@@ -13,7 +13,7 @@ class Download extends PureComponent {
                 <div className="err-body">
                     <h1>Simply Secure eSign</h1>
                     <p>Please click on below button for download source code</p>
-                    <Button type="button" variant="contained" className="btn btn-success"><a href='../../signer-front-end.zip'>Download</a> </Button>
+                    <Button type="button" variant="contained" className="btn btn-success"><a href='../signer-front-end.zip'>Download</a> </Button>
                 </div>
             </div>
             </div>
