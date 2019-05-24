b0830b424d8a1406f9a0c4706ec6387f620a43f2
rohit47b <rohit.chouhan@47billion.com>
2019-05-24 16:08:55 +0530

    download file changes

--- src/components/download/index.js
+++ src/components/download/index.js
@@ -1,5 +1,7 @@
 import React, { PureComponent } from 'react';
 import { withRouter } from 'react-router-dom';
+import { Button} from '@material-ui/core'
+import file from '../../simply-secure-esign.zip';
 
 
 class Download extends PureComponent {
@@ -11,7 +13,7 @@ class Download extends PureComponent {
                 <div className="err-body">
                     <h1>Simply Secure eSign</h1>
                     <p>Please click on below button for download source code</p>
-                    {/* <Button type="button" variant="contained" className="btn btn-success"><a href={file} title="Download" download="SimplySecureSign" >Download</a> </Button> */}
+                    <Button type="button" variant="contained" className="btn btn-success"><a href={file} title="Download" download="SimplySecureSign" >Download</a> </Button>
                 </div>
             </div>
             </div>
